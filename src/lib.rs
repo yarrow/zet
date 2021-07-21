aea9e16efede485227cb854ee541554ea6ff6f90
Yarrow Angelweed <yarrow.angelweed@gmail.com>
2021-07-20 20:18:57 -0400

    Only `deny(warnings)` for release builds

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
 //! The `calculate::exec` function is the kernel of the appliction.  The `args` module parses
 //! the command line, and the `io` module hides I/O details.
 
-#![cfg_attr(debug_assertions, allow(dead_code, unused_imports))]
+#![deny(warnings)]
+#![cfg_attr(debug_assertions, allow(dead_code, unused_imports), warn(warnings))]
 #![deny(unused_must_use)]
 #![deny(clippy::all)]
 #![allow(clippy::needless_return)]
 #![deny(clippy::pedantic)]
 #![allow(clippy::missing_errors_doc)]
-#![deny(warnings)]
 #![deny(
     missing_docs,
     trivial_casts,
