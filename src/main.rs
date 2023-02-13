9be24bd3899bdfe70beb71e944796d8b66439e89
Yarrow Angelweed <yarrow.angelweed@gmail.com>
2023-02-13 01:46:15 -0500

    Print multiple colors (not semantically meaningful)

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,23 @@ struct Chunk<'a> {
 fn main() {
     let input = include_str!("help.txt");
     let help = parse(input);
-    println!("{}", help.style.style(help.text));
+    for line in help {
+        println!("{}", line.style.style(line.text));
+    }
 }
 
-fn parse<'a>(text: &'a str) -> Chunk<'a> {
-    Chunk {
-        style: Style::new().blue(),
-        text,
-    }
+fn parse<'a>(text: &'a str) -> Vec<Chunk<'a>> {
+    let color = vec![Style::new().blue(), Style::new().green()];
+    let mut c = 0;
+    let lines = text
+        .lines()
+        .map(|line| {
+            c = (c + 1) & 1;
+            Chunk {
+                style: color[c],
+                text: line,
+            }
+        })
+        .collect();
+    lines
 }
