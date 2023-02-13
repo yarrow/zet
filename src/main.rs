8fdc4e455ed7242da2427e85c466359cc18382a5
Yarrow Angelweed <yarrow.angelweed@gmail.com>
2023-02-13 01:30:29 -0500

    Print help in one (just one!) color

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,20 @@
+//use owo_colors::{OwoColorize, Stream::Stdout, Style, Styled};
+use owo_colors::Style;
+
+struct Chunk<'a> {
+    style: Style,
+    text: &'a str,
+}
+
 fn main() {
-    let help = include_str!("help.txt");
-    println!("{help}");
+    let input = include_str!("help.txt");
+    let help = parse(input);
+    println!("{}", help.style.style(help.text));
+}
+
+fn parse<'a>(text: &'a str) -> Chunk<'a> {
+    Chunk {
+        style: Style::new().blue(),
+        text,
+    }
 }
