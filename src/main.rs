75db779ef58e5816a3cf1cc1c96674f65730c8c8
Yarrow Angelweed <yarrow.angelweed@gmail.com>
2023-02-15 12:01:36 -0500

    Make term_width and styles global constants
    Well, global once_cell values

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,36 @@
 #![allow(clippy::missing_errors_doc, clippy::semicolon_if_nothing_returned)]
 #![cfg_attr(debug_assertions, allow(dead_code, unused_imports, unused_variables))]
 
-//use owo_colors::{OwoColorize, Stream::Stdout, Style, Styled};
+use once_cell::sync::Lazy;
 use owo_colors::Style;
 use terminal_size::{terminal_size, Height, Width};
+
+struct Constants {
+    term_width: usize,
+    plain: Style,
+    heading: Style,
+    entry: Style,
+}
+
+static C: Lazy<Constants> = Lazy::new(|| {
+    fn inner() -> Option<usize> {
+        std::env::var_os("COLUMNS")?.to_str()?.parse::<usize>().ok()
+    }
+    let term_width = if let Some((Width(width), Height(_))) = terminal_size() {
+        width as usize
+    } else {
+        inner().unwrap_or(80)
+    };
+    let plain = Style::new();
+    let heading = Style::new().yellow();
+    let entry = Style::new().green();
+    Constants {
+        term_width,
+        plain,
+        heading,
+        entry,
+    }
+});
 
 struct Entry<'a> {
     item: &'a str,
@@ -41,38 +68,28 @@ impl<'a> Section<'a> {
         }
     }
     fn next_line_help(&self) -> bool {
-        let term_width = term_width();
         self.entries
             .iter()
-            .any(|e| e.item.len() + e.caption.len() > term_width)
+            .any(|e| e.item.len() + e.caption.len() > C.term_width)
     }
 }
 struct Help<'a>(Vec<Part<'a>>);
 
-fn term_width() -> usize {
-    fn inner() -> Option<usize> {
-        std::env::var_os("COLUMNS")?.to_str()?.parse::<usize>().ok()
-    }
-    if let Some((Width(width), Height(_))) = terminal_size() {
-        width as usize
-    } else {
-        inner().unwrap_or(80)
-    }
-}
 impl<'a> Help<'a> {
     fn print(&'a self) {
-        let term_width = term_width();
-        let plain = Style::new();
-        let heading = Style::new().yellow();
-        let entry = Style::new().green();
         let version = std::env!("CARGO_PKG_VERSION");
-        let name = entry.bold().style("zet");
-        println!("{} {}", name, plain.style(version));
+        let name = C.entry.bold().style("zet");
+        println!("{} {}", name, C.plain.style(version));
         for line in &self.0 {
             match line {
-                Part::Paragraph(text) => println!("{}", plain.style(text)),
+                Part::Paragraph(text) => println!("{}", C.plain.style(text)),
                 Part::Usage(args) => {
-                    println!("{}{}{}", heading.style("Usage: "), name, plain.style(args))
+                    println!(
+                        "{}{}{}",
+                        C.heading.style("Usage: "),
+                        name,
+                        C.plain.style(args)
+                    )
                 }
                 Part::Section(s) => {
                     let between = if s.next_line_help() {
@@ -80,9 +97,14 @@ impl<'a> Help<'a> {
                     } else {
                         ""
                     };
-                    println!("{}", heading.style(s.title));
+                    println!("{}", C.heading.style(s.title));
                     for Entry { item, caption } in &s.entries {
-                        println!("{}{}{}", entry.style(item), between, plain.style(caption));
+                        println!(
+                            "{}{}{}",
+                            C.entry.style(item),
+                            between,
+                            C.plain.style(caption)
+                        );
                     }
                 }
             };
